8a8225850d080bbaa3b1bdb688da59ba017aa13a
GITHUB Nexend <GITHUB ddante@ukr.net>
2021-02-02 14:11:17 +0300

    task-fix

--- react/lesson2/src/Pagination.jsx
+++ react/lesson2/src/Pagination.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
 
-// текущая страница , всего элементов в списке, кол-во эл на странице
-
 const Pagination = ({ goPrev, goNext, currentPage, totalItems, itemsPerPage }) => {
   const isPrevPageAvailable = currentPage > 1;
-  const isNextPageAvailable = totalItems / itemsPerPage > currentPage;
+  const isNextPageAvailable = Math.ceil(totalItems / itemsPerPage) > currentPage;
   return (
     <div className="pagination">
       <button className="btn" onClick={goPrev} disabled={!isPrevPageAvailable}>
